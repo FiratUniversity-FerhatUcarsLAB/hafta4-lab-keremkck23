b00662a89aeaa7125ada2ebd7071e89d7bfb652d
Kerem <keremkucuk023@gmail.com>
2025-10-27 01:23:33 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -47,15 +47,20 @@ public class OgrenciBilgi {
 
         System.out.println("Öğrenci Bilgi Sistemi");
         System.out.println("=================");
+     
         System.out.println("Ad Soyad:"+ad+(" ")+soyad);
         System.out.println("Öğrenci No: "+ogrencino);
+     
         System.out.println("Yaş:"+yas);
         System.out.printf("GPA: %.2f",gpa);
+     
         System.out.println();
         if (gpa>=3.00) {
             System.out.println("Durum: Başarılı");
         }else{
             System.out.println("Durum: Başarısız");}
+     
+     //Sistemi Kapatma
         input.close();
 
 
